41c5ba4180c915ce9db57a685da90608eb9fe9bf
Ignace Philippe Nyamagana Butera <nyamsprod@gmail.com>
2018-10-03 11:22:12 +0200

    improve custom.js

--- docs/custom.js
+++ docs/custom.js
@@ -1,16 +1,21 @@
 (() => {
+  let uri = new URL(location.href);
   let versions = document.querySelector('.versions');
   versions.addEventListener(
     'change',
-    () => location.href = versions.options[versions.options.selectedIndex].dataset.url
+    () => {
+      uri.hash = '';
+      uri.pathname = versions.options[versions.options.selectedIndex].dataset.url;
+      location.href = uri.toString();
+    }
   , false);
 
-  let uri = location.href.split('#', 2).pop();
   document.querySelector('main').querySelectorAll("h2[id]").forEach((header) => {
+    uri.hash = header.id;
     let link = document.createElement("a");
     link.className = "header-permalink";
     link.title = "Permalink";
-    link.href = uri + '#' + header.id;
+    link.href = uri.toString();
     link.innerHTML = "&#182;";
     header.appendChild(link);
   });
