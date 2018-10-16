98fe33337989649f8601ea7a38875c7e4048b4f9
Ignace Philippe Nyamagana Butera <nyamsprod@gmail.com>
2018-10-16 09:06:39 +0200

    improve helper functions

--- src/functions.php
+++ src/functions.php
@@ -37,10 +37,14 @@ use function sprintf;
 /**
  * Returns a DateTimeImmutable object.
  *
- * @param mixed $year_or_datepoint the number of year
- *                                 or a DateTimeInterface object
- *                                 or a string parsable by DateTime::__construct
- *                                 or a timestamp as an integer
+ * A datepoint can be
+ * <ul>
+ * <li>a DateTimeInterface object
+ * <li>a string parsable by DateTime::__construct
+ * <li>a integer interpreted as a timestamp
+ * </ul>
+ *
+ * @param mixed $year_or_datepoint a year as an int or a datepoint
  */
 function datepoint(
     $year_or_datepoint,
@@ -109,8 +113,8 @@ function duration($duration): DateInterval
         return $duration;
     }
 
-    if (false !== ($res = filter_var($duration, FILTER_VALIDATE_INT))) {
-        return new DateInterval('PT'.$res.'S');
+    if (false !== ($second = filter_var($duration, FILTER_VALIDATE_INT))) {
+        return new DateInterval('PT'.$second.'S');
     }
 
     if (is_string($duration)) {
@@ -129,8 +133,9 @@ function duration($duration): DateInterval
 function interval_after($datepoint, $duration): Period
 {
     $datepoint = datepoint($datepoint);
+    $duration = duration($duration);
 
-    return new Period($datepoint, $datepoint->add(duration($duration)));
+    return new Period($datepoint, $datepoint->add($duration));
 }
 
 /**
@@ -139,8 +144,9 @@ function interval_after($datepoint, $duration): Period
 function interval_before($datepoint, $duration): Period
 {
     $datepoint = datepoint($datepoint);
+    $duration = duration($duration);
 
-    return new Period($datepoint->sub(duration($duration)), $datepoint);
+    return new Period($datepoint->sub($duration), $datepoint);
 }
 
 /**
@@ -176,18 +182,18 @@ function interval_from_dateperiod(DatePeriod $datePeriod): Period
 /**
  * Creates new instance for a specific year.
  *
- * @param mixed $int_or_datepoint a year as an int or a datepoint
+ * @param mixed $year_or_datepoint a year as an int or a datepoint
  */
-function year($int_or_datepoint): Period
+function year($year_or_datepoint): Period
 {
-    if (!is_int($int_or_datepoint)) {
-        $datepoint = datepoint($int_or_datepoint);
-        $startDate = $datepoint->setTime(0, 0)->setDate((int) $datepoint->format('Y'), 1, 1);
+    if (!is_int($year_or_datepoint)) {
+        $datepoint = datepoint($year_or_datepoint);
+        $startDate = $datepoint->setDate((int) $datepoint->format('Y'), 1, 1)->setTime(0, 0);
 
         return new Period($startDate, $startDate->add(new DateInterval('P1Y')));
     }
 
-    $startDate = datepoint($int_or_datepoint, 1, 1);
+    $startDate = datepoint($year_or_datepoint, 1, 1);
 
     return new Period($startDate, $startDate->add(new DateInterval('P1Y')));
 }
@@ -195,38 +201,33 @@ function year($int_or_datepoint): Period
 /**
  * Creates new instance for a specific ISO year.
  *
- * @param mixed $int_or_datepoint a year as an int or a datepoint
+ * @param mixed $year_or_datepoint a year as an int or a datepoint
  */
-function iso_year($int_or_datepoint): Period
+function iso_year($year_or_datepoint): Period
 {
-    if (!is_int($int_or_datepoint)) {
-        $datepoint = datepoint($int_or_datepoint)->setTime(0, 0);
-        $int_or_datepoint = (int) $datepoint->format('o');
+    if (!is_int($year_or_datepoint)) {
+        $datepoint = datepoint($year_or_datepoint)->setTime(0, 0);
+        $iso_year = (int) $datepoint->format('o');
 
-        return new Period(
-            $datepoint->setISODate($int_or_datepoint, 1),
-            $datepoint->setISODate(++$int_or_datepoint, 1)
-        );
+        return new Period($datepoint->setISODate($iso_year, 1), $datepoint->setISODate(++$iso_year, 1));
     }
 
-    $datepoint = datepoint($int_or_datepoint, 1, 1);
-
     return new Period(
-        $datepoint->setISODate($int_or_datepoint, 1),
-        $datepoint->setISODate(++$int_or_datepoint, 1)
+        (new DateTimeImmutable())->setISODate($year_or_datepoint, 1)->setTime(0, 0),
+        (new DateTimeImmutable())->setISODate(++$year_or_datepoint, 1)->setTime(0, 0)
     );
 }
 
 /**
  * Creates new instance for a specific semester in a given year.
  *
- * @param mixed $int_or_datepoint a year as an int or a datepoint
- * @param int   $index            a semester index from 1 to 2 included
+ * @param mixed $year_or_datepoint a year as an int or a datepoint
+ * @param int   $index             a semester index from 1 to 2 included
  */
-function semester($int_or_datepoint, int $index = 1): Period
+function semester($year_or_datepoint, int $index = 1): Period
 {
-    if (!is_int($int_or_datepoint)) {
-        $datepoint = datepoint($int_or_datepoint);
+    if (!is_int($year_or_datepoint)) {
+        $datepoint = datepoint($year_or_datepoint);
         $startDate = $datepoint->setTime(0, 0)->setDate(
             (int) $datepoint->format('Y'),
             (intdiv((int) $datepoint->format('n'), 6) * 6) + 1,
@@ -236,7 +237,7 @@ function semester($int_or_datepoint, int $index = 1): Period
         return new Period($startDate, $startDate->add(new DateInterval('P6M')));
     }
 
-    $startDate = datepoint($int_or_datepoint, (($index - 1) * 6) + 1, 1);
+    $startDate = datepoint($year_or_datepoint, (($index - 1) * 6) + 1, 1);
 
     return new Period($startDate, $startDate->add(new DateInterval('P6M')));
 }
@@ -244,13 +245,13 @@ function semester($int_or_datepoint, int $index = 1): Period
 /**
  * Creates new instance for a specific quarter in a given year.
  *
- * @param mixed $int_or_datepoint a year as an int or a datepoint
- * @param int   $index            quarter index
+ * @param mixed $year_or_datepoint a year as an int or a datepoint
+ * @param int   $index             quarter index
  */
-function quarter($int_or_datepoint, int $index = 1): Period
+function quarter($year_or_datepoint, int $index = 1): Period
 {
-    if (!is_int($int_or_datepoint)) {
-        $datepoint = datepoint($int_or_datepoint);
+    if (!is_int($year_or_datepoint)) {
+        $datepoint = datepoint($year_or_datepoint);
         $startDate = $datepoint->setTime(0, 0)->setDate(
             (int) $datepoint->format('Y'),
             (intdiv((int) $datepoint->format('n'), 3) * 3) + 1,
@@ -260,7 +261,7 @@ function quarter($int_or_datepoint, int $index = 1): Period
         return new Period($startDate, $startDate->add(new DateInterval('P3M')));
     }
 
-    $startDate = datepoint($int_or_datepoint, (($index - 1) * 3) + 1, 1);
+    $startDate = datepoint($year_or_datepoint, (($index - 1) * 3) + 1, 1);
 
     return new Period($startDate, $startDate->add(new DateInterval('P3M')));
 }
@@ -268,19 +269,23 @@ function quarter($int_or_datepoint, int $index = 1): Period
 /**
  * Creates new instance for a specific year and month.
  *
- * @param mixed $int_or_datepoint a year as an int or a datepoint
- * @param int   $index            month index from 1 to 12 included
+ * @param mixed $year_or_datepoint a year as an int or a datepoint
+ * @param int   $index             month index from 1 to 12 included
  */
-function month($int_or_datepoint, int $index = 1): Period
+function month($year_or_datepoint, int $index = 1): Period
 {
-    if (!is_int($int_or_datepoint)) {
-        $datepoint = datepoint($int_or_datepoint);
-        $startDate = $datepoint->setTime(0, 0)->setDate((int) $datepoint->format('Y'), (int) $datepoint->format('n'), 1);
+    if (!is_int($year_or_datepoint)) {
+        $datepoint = datepoint($year_or_datepoint);
+        $startDate = $datepoint->setTime(0, 0)->setDate(
+            (int) $datepoint->format('Y'),
+            (int) $datepoint->format('n'),
+            1
+        );
 
         return new Period($startDate, $startDate->add(new DateInterval('P1M')));
     }
 
-    $startDate = datepoint($int_or_datepoint, $index, 1);
+    $startDate = datepoint($year_or_datepoint, $index, 1);
 
     return new Period($startDate, $startDate->add(new DateInterval('P1M')));
 }
@@ -288,19 +293,19 @@ function month($int_or_datepoint, int $index = 1): Period
 /**
  * Creates new instance for a specific ISO8601 week.
  *
- * @param mixed $int_or_datepoint a year as an int or a datepoint
- * @param int   $index            iso week index
+ * @param mixed $year_or_datepoint a year as an int or a datepoint
+ * @param int   $index             iso week index
  */
-function iso_week($int_or_datepoint, int $index = 1): Period
+function iso_week($year_or_datepoint, int $index = 1): Period
 {
-    if (!is_int($int_or_datepoint)) {
-        $datepoint = datepoint($int_or_datepoint)->setTime(0, 0);
+    if (!is_int($year_or_datepoint)) {
+        $datepoint = datepoint($year_or_datepoint)->setTime(0, 0);
         $startDate = $datepoint->setISODate((int) $datepoint->format('o'), (int) $datepoint->format('W'), 1);
 
         return new Period($startDate, $startDate->add(new DateInterval('P7D')));
     }
 
-    $startDate = (new DateTimeImmutable())->setTime(0, 0)->setISODate($int_or_datepoint, $index, 1);
+    $startDate = (new DateTimeImmutable())->setTime(0, 0)->setISODate($year_or_datepoint, $index, 1);
 
     return new Period($startDate, $startDate->add(new DateInterval('P7D')));
 }
@@ -311,17 +316,17 @@ function iso_week($int_or_datepoint, int $index = 1): Period
  * The date is truncated so that the time range starts at midnight
  * according to the date timezone and last a full day.
  *
- * @param mixed $int_or_datepoint a year as an int or a datepoint
+ * @param mixed $year_or_datepoint a year as an int or a datepoint
  */
-function day($int_or_datepoint, int $month = 1, int $day = 1): Period
+function day($year_or_datepoint, int $month = 1, int $day = 1): Period
 {
-    if (!is_int($int_or_datepoint)) {
-        $startDate = datepoint($int_or_datepoint)->setTime(0, 0);
+    if (!is_int($year_or_datepoint)) {
+        $startDate = datepoint($year_or_datepoint)->setTime(0, 0);
 
         return new Period($startDate, $startDate->add(new DateInterval('P1D')));
     }
 
-    $startDate = datepoint($int_or_datepoint, $month, $day);
+    $startDate = datepoint($year_or_datepoint, $month, $day);
 
     return new Period($startDate, $startDate->add(new DateInterval('P1D')));
 }
@@ -332,16 +337,16 @@ function day($int_or_datepoint, int $month = 1, int $day = 1): Period
  * The starting datepoint represents the beginning of the hour
  * The interval is equal to 1 hour
  */
-function hour($int_or_datepoint, int $month = 1, int $day = 1, int $hour = 0): Period
+function hour($year_or_datepoint, int $month = 1, int $day = 1, int $hour = 0): Period
 {
-    if (!is_int($int_or_datepoint)) {
-        $datepoint = datepoint($int_or_datepoint);
+    if (!is_int($year_or_datepoint)) {
+        $datepoint = datepoint($year_or_datepoint);
         $startDate = $datepoint->setTime((int) $datepoint->format('H'), 0);
 
         return new Period($startDate, $startDate->add(new DateInterval('PT1H')));
     }
 
-    $startDate = datepoint($int_or_datepoint, $month, $day, $hour);
+    $startDate = datepoint($year_or_datepoint, $month, $day, $hour);
 
     return new Period($startDate, $startDate->add(new DateInterval('PT1H')));
 }
@@ -352,16 +357,16 @@ function hour($int_or_datepoint, int $month = 1, int $day = 1, int $hour = 0): P
  * The starting datepoint represents the beginning of the minute
  * The interval is equal to 1 minute
  */
-function minute($int_or_datepoint, int $month = 1, int $day = 1, int $hour = 0, int $minute = 0): Period
+function minute($year_or_datepoint, int $month = 1, int $day = 1, int $hour = 0, int $minute = 0): Period
 {
-    if (!is_int($int_or_datepoint)) {
-        $datepoint = datepoint($int_or_datepoint);
+    if (!is_int($year_or_datepoint)) {
+        $datepoint = datepoint($year_or_datepoint);
         $startDate = $datepoint->setTime((int) $datepoint->format('H'), (int) $datepoint->format('i'));
 
         return new Period($startDate, $startDate->add(new DateInterval('PT1M')));
     }
 
-    $startDate = datepoint($int_or_datepoint, $month, $day, $hour, $minute);
+    $startDate = datepoint($year_or_datepoint, $month, $day, $hour, $minute);
 
     return new Period($startDate, $startDate->add(new DateInterval('PT1M')));
 }
@@ -372,10 +377,10 @@ function minute($int_or_datepoint, int $month = 1, int $day = 1, int $hour = 0, 
  * The starting datepoint represents the beginning of the second
  * The interval is equal to 1 second
  */
-function second($int_or_datepoint, int $month = 1, int $day = 1, int $hour = 0, int $minute = 0, int $second = 0): Period
+function second($year_or_datepoint, int $month = 1, int $day = 1, int $hour = 0, int $minute = 0, int $second = 0): Period
 {
-    if (!is_int($int_or_datepoint)) {
-        $datepoint = datepoint($int_or_datepoint);
+    if (!is_int($year_or_datepoint)) {
+        $datepoint = datepoint($year_or_datepoint);
         $startDate = $datepoint->setTime(
             (int) $datepoint->format('H'),
             (int) $datepoint->format('i'),
@@ -385,7 +390,7 @@ function second($int_or_datepoint, int $month = 1, int $day = 1, int $hour = 0, 
         return new Period($startDate, $startDate->add(new DateInterval('PT1S')));
     }
 
-    $startDate = datepoint($int_or_datepoint, $month, $day, $hour, $minute, $second);
+    $startDate = datepoint($year_or_datepoint, $month, $day, $hour, $minute, $second);
 
     return new Period($startDate, $startDate->add(new DateInterval('PT1S')));
 }
